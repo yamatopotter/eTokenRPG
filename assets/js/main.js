46213c0a90a8e92ecc58682504246e22bd7fdc21
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2021-11-27 01:45:59 -0300

    add: Validação da data de nascimento

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,15 @@
 console.log("MAIN.JS - CARREGADO");
 
 var skillCheckboxes = document.querySelectorAll(".skillCheckbox");
+var birthdatePlayer = document.getElementById("dataNascimento");
 let skillLimit = 3;
 
-console.log(skillCheckboxes.length);
+// console.log(skillCheckboxes.length);
 
+// Adição de função onClick usando JS
 for (let i=0; i < skillCheckboxes.length; i++){
     skillCheckboxes[i].onclick = skillLimitSelection;
-    console.log("A checkbox de numero "+i+" foi adicionada a função");
+    // console.log("A checkbox de numero "+i+" foi adicionada a função");
 }
 
 function skillLimitSelection(event){
@@ -20,4 +22,16 @@ function skillLimitSelection(event){
     else{
         console.log("Habilidade adicionada/removida à lista");
     }
+}
+
+function olderThan18(event){
+    let datePlayer = new Date(birthdatePlayer.value);
+    let dateNow = new Date();
+
+    if(datePlayer.getFullYear()+18>dateNow.getFullYear()){
+        alert("Você não pode usar o sistema pois possui menos de 18 anos");
+        document.getElementById("btnCadastrar").disabled = true;
+    }
+
+    console.log(datePlayer.getFullYear());
 }
